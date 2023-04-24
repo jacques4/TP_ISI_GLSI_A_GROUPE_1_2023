3294281a07b6b6511eb01b393b2ea5f17af34bb8
Jacques ASSIH <assihjacques5@gmail.com>
2023-04-24 16:29:22 +0000

    initial commit

---
+++ src/main/java/com/iai/togo/ProjetBanque/ProjetBanqueApplication.java
@@ -0,0 +1,13 @@
+package com.iai.togo.ProjetBanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBanqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iai/togo/ProjetBanque/ProjetBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iai.togo.ProjetBanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
